b64cfe9ece33b317acf0fd32805c5ce7441e425d
Sandesh_Gautam <xsandeshgautamx@gmail.com>
2024-07-16 16:22:30 +0545

    Dashboard

--- MarketingBlogApp/Pages/Admin/Dashboard.cshtml.cs
+++ MarketingBlogApp/Pages/Admin/Dashboard.cshtml.cs
@@ -5,10 +5,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
 
 namespace MarketingBlogApp.Pages.Admin
 {
@@ -22,9 +22,15 @@ namespace MarketingBlogApp.Pages.Admin
         public int TotalPosts { get; set; }
         public int TotalCategories { get; set; }
         public int TotalVisits { get; set; }
+        public int TotalUsers { get; set; }
+        public int TotalManagers { get; set; }
 
         public IList<ManagerAction> ManagerActions { get; set; }
         public IList<Warning> Warnings { get; set; }
+
+        public List<ChartData> PostsOverTime { get; set; }
+        public List<ChartData> UserGrowth { get; set; }
+        public List<ChartData> VisitsOverTime { get; set; }
 
         public DashboardModel(ILogger<DashboardModel> logger, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
         {
@@ -38,13 +44,16 @@ namespace MarketingBlogApp.Pages.Admin
             TotalPosts = await _context.BlogPosts.CountAsync();
             TotalCategories = await _context.Categories.CountAsync();
             TotalVisits = await _context.UserActivities.CountAsync(ua => ua.ActivityType == "Page Visit");
+            TotalUsers = await _userManager.Users.CountAsync();
+            TotalManagers = (await _userManager.GetUsersInRoleAsync("Manager")).Count;
 
             ManagerActions = await _context.ManagerActions
                 .Include(ma => ma.Manager)
                 .ToListAsync();
+
             Warnings = await _context.Warnings
-                            .Include(w => w.User)
-                            .ToListAsync();
+                .Include(w => w.User)
+                .ToListAsync();
 
             var user = await _userManager.GetUserAsync(User);
             if (user != null)
@@ -58,6 +67,40 @@ namespace MarketingBlogApp.Pages.Admin
                 _context.UserActivities.Add(userActivity);
                 await _context.SaveChangesAsync();
             }
+
+            PostsOverTime = await _context.BlogPosts
+                .GroupBy(p => new { p.PublishedDate.Year, p.PublishedDate.Month })
+                .Select(g => new ChartData
+                {
+                    Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+                    Count = g.Count()
+                })
+                .ToListAsync();
+
+            UserGrowth = await _userManager.Users
+                .GroupBy(u => new { u.CreatedAt.Year, u.CreatedAt.Month })
+                .Select(g => new ChartData
+                {
+                    Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+                    Count = g.Count()
+                })
+                .ToListAsync();
+
+            VisitsOverTime = await _context.UserActivities
+                .Where(ua => ua.ActivityType == "Page Visit")
+                .GroupBy(ua => new { ua.ActivityDate.Year, ua.ActivityDate.Month })
+                .Select(g => new ChartData
+                {
+                    Date = new DateTime(g.Key.Year, g.Key.Month, 1),
+                    Count = g.Count()
+                })
+                .ToListAsync();
         }
+    }
+
+    public class ChartData
+    {
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
     }
 }
